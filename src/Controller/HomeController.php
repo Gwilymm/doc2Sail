5f784c3e11f399eb24f8f499ad699c1718d36499
Gwilymm <marin.davies@gmail.com>
2025-10-22 20:40:03 +0200

    feat(routes): remove locale prefix configuration and update HomeController to simplify routing feat(navbar): update language selection links to use named routes for better clarity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
 	#[Route('/', name: 'app_home')]
-	// la route peut être préfixée par /{_locale} grâce à config/routes.yaml
-	public function index(Request $request): Response
+	public function index(): Response
 	{
-		$locale = $request->getLocale();
 		if ($this->isGranted('ROLE_USER')) {
 			return $this->redirectToRoute('app_regatta');
 		}
