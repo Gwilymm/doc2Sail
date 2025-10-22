b8ecaf39ead94cda84eebe078910ab620e10d130
Gwilymm <marin.davies@gmail.com>
2025-10-22 20:09:41 +0200

    feat(routes): make locale prefix optional and set default locale in routing configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
 	#[Route('/', name: 'app_home')]
-	public function index(): Response
+	// la route peut être préfixée par /{_locale} grâce à config/routes.yaml
+	public function index(Request $request): Response
 	{
+		$locale = $request->getLocale();
 		if ($this->isGranted('ROLE_USER')) {
 			return $this->redirectToRoute('app_regatta');
 		}
