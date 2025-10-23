090b0049aa70f5b629fd470d7b555ae11c7b519b
Gwilymm <marin.davies@gmail.com>
2025-10-23 08:40:28 +0200

    feat(qrcode): update QR code modal and sharing functionality to include locale in URLs; add custom 404 error page with animations

---
+++ src/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/TestController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+	#[Route('/test-404', name: 'test_404')]
+	public function test404(): Response
+	{
+		return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
+	}
+}
